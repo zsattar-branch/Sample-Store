106dbfcff86ac26a436db37b73fab131da451dea
zsattar-branch <zain.sattar@branch.io>
2023-02-27 14:05:13 -0500

    Basic Branch functionality

--- FlowerStore/AppDelegate.swift
+++ FlowerStore/AppDelegate.swift
@@ -7,16 +7,52 @@
 
 import UIKit
 import DSKit
+import BranchSDK
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
         DSAppearance.shared.main = PeachAppearance()
         DSAppearance.shared.userInterfaceStyle = .light
+        
+      // if you are using the TEST key
+      //Branch.setUseTestBranchKey(false)
+      // listener for Branch Deep Link data
+        
+      Branch.getInstance().enableLogging()
+
+      Branch.getInstance().initSession(launchOptions: launchOptions) { (params, error) in
+           // do stuff with deep link data (nav to page, display content, etc)
+          print(params as? [String: AnyObject] ?? {})
+      }
+      return true
+    }
+    
+//    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+//        DSAppearance.shared.main = PeachAppearance()
+//        DSAppearance.shared.userInterfaceStyle = .light
+//        return true
+//    }
+
+    
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        Branch.getInstance().application(app, open: url, options: options)
         return true
     }
 
+    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+      // handler for Universal Links
+        Branch.getInstance().continue(userActivity)
+        return true
+    }
+
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+      // handler for Push Notifications
+      Branch.getInstance().handlePushNotification(userInfo)
+    }
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
